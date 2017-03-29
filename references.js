857677fd638f4413ce4d029bd12dbf700a761f78
Ryo <kanadajinlee@yahoo.ca>
2017-03-29 16:04:26 -0500

    Was expecting a more educational/effective result, but at least now (これは＃２６３でやったんだけど) I can see how we may go about modularizing YelpCamp

--- references.js
+++ references.js
@@ -6,7 +6,6 @@ mongoose.connect("mongodb://localhost/blog_demo_2");
 let Post = require("./models/post");
 let User = require("./models/user");
 
-
 /* Create a user */
 // User.create({
 //     email: "bob@gmail.com",
@@ -14,25 +13,25 @@ let User = require("./models/user");
 // });
 
 /* Create a post */
-// Post.create({
-//     title: "How to cook the best burger Pt. 3",
-//     content: "kajsdlratkjahrtihtijhgirghiueahgeiugh"
-// }, (err, post) => {
-//     User.findOne({email: "bob@gmail.com"}, (err, foundUser) => {
-//         if(err){
-//             console.log("error: " + err);
-//         } else {
-//             foundUser.posts.push(post);
-//             foundUser.save((err, data) => {
-//               if(err){
-//                   console.log("err: " + err);
-//               } else {
-//                   console.log("data: " + data)
-//               }
-//             });
-//         }
-//     });
-// });
+Post.create({
+    title: "How to cook the best burger Pt. 4",
+    content: "Write a book and get lotsa money"
+}, (err, post) => {
+    User.findOne({email: "bob@gmail.com"}, (err, foundUser) => {
+        if(err){
+            console.log("error: " + err);
+        } else {
+            foundUser.posts.push(post);
+            foundUser.save((err, data) => {
+              if(err){
+                  console.log("err: " + err);
+              } else {
+                  console.log("data: " + data)
+              }
+            });
+        }
+    });
+});
 
 /* Find a user, & all of that user's posts */
 // User.findOne({email: "bob@gmail.com"}).populate("posts").exec((err, user) => {
